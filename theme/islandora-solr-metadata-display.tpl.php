ff2dfb51f579fe277ad2138ed78f372b23ebf4a8
Mark <mbagget1@utk.edu>
2015-07-27 09:55:05 -0400

    Modify tpl file to pull specific elements out of SOLR and put into the DOM

--- theme/islandora-solr-metadata-display.tpl.php
+++ theme/islandora-solr-metadata-display.tpl.php
@@ -24,16 +24,122 @@
   <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
   <div class="fieldset-wrapper">
     <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields">
-      <?php $row_field = 0; ?>
-      <?php foreach($solr_fields as $value): ?>
-        <dt class="<?php print $row_field == 0 ? ' first' : ''; ?>">
-          <?php print $value['display_label']; ?>
-        </dt>
-        <dd class="<?php print $row_field == 0 ? ' first' : ''; ?>">
-          <?php print check_markup(implode("\n", $value['value']), 'islandora_solr_metadata_filtered_html'); ?>
-        </dd>
-        <?php $row_field++; ?>
-      <?php endforeach; ?>
+ <?php
+	      dpm($solr_fields);
+	      if(isset($solr_fields['mods_titleInfo_title_s']['value'])){
+		      print "<dt class='first'>Title</dt><dd class='first'><p>" . $solr_fields['mods_titleInfo_title_s']['value'][0] . "</p></dd>";
+	      }
+	      if(isset($solr_fields['mods_identifier_local_s']['value'])){
+		      print "<dt>Identifiers</dt><dd><p>" . $solr_fields['mods_identifier_local_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_name_creator_namePart_ms']['value'])){
+		      print "<dt>Creator</dt><dd>";
+		      foreach(array_combine($solr_fields['mods_name_creator_namePart_ms']['value'], $solr_fields['mods_name_role_roleTerm_text_ms']['value']) as $value => $nameRole){
+				  print "<p>" . $value . " (" . $nameRole . ")</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if(isset($solr_fields['mods_originInfo_dateCreated_s']['value'])){
+		      print "<dt>Date of Creation</dt><dd><p>" . $solr_fields['mods_originInfo_dateCreated_s']['value'][0] . "</p></dd>";
+	      }
+	      if(isset($solr_fields['mods_originInfo_dateIssued_ms']['value'])){
+		      print "<dt>Date of Publication</dt><dd><p>" . $solr_fields['mods_originInfo_dateIssued_ms']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_originInfo_publisher_s']['value'])){
+		      print "<dt>Publisher</dt><dd><p>" . $solr_fields['mods_originInfo_publisher_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_originInfo_publisher_s']['value'])){
+		      print "<dt>Publisher</dt><dd><p>" . $solr_fields['mods_originInfo_publisher_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_originInfo_place_placeTerm_text_s']['value'])){
+		      print "<dt>Place of Origin</dt><dd><p>" . $solr_fields['mods_originInfo_place_placeTerm_text_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_physicalDescription_extent_s']['value'])){
+		      print "<dt>Extent</dt><dd><p>" . $solr_fields['mods_physicalDescription_extent_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_abstract_ms']['value'])){
+		      print "<dt>Abstract / Descriptive Note</dt><dd>";
+		      foreach($solr_fields['mods_abstract_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_subject_authority_lcsh_topic_ms']['value'])){
+		      print "<dt>Subject Topic (LCSH)</dt><dd>";
+		      foreach($solr_fields['mods_subject_authority_lcsh_topic_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_subject_authority_dots_topic_ms']['value'])){
+		      print "<dt>Subject Topic (DOTS)</dt><dd>";
+		      foreach($solr_fields['mods_subject_authority_dots_topic_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_subject_name_namePart_ms']['value'])){
+		      print "<dt>Subject Name</dt><dd>";
+		      foreach($solr_fields['mods_subject_name_namePart_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_subject_geographic_ms']['value'])){
+		      print "<dt>Subject Geographic</dt><dd>";
+		      foreach(array_combine($solr_fields['mods_subject_geographic_ms']['value'], $solr_fields['mods_subject_cartographics_coordinates_ms']['value']) as $value => $cartographic){
+				  print "<p>" . $value . " (" . $cartographic . ")</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_physicalDescription_form_s']['value'])){
+		      print "<dt>Form</dt><dd><p>" . $solr_fields['mods_physicalDescription_form_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_genre_s']['value'])){
+		      print "<dt>Genre</dt><dd><p>" . $solr_fields['mods_genre_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_language_languageTerm_code_s']['value'])){
+		      print "<dt>Language of Resource</dt><dd><p>" . $solr_fields['mods_language_languageTerm_code_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_note_ms']['value'])){
+		      print "<dt>Note</dt><dd>";
+		      foreach($solr_fields['mods_note_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_accessCondition_ms']['value'])){
+		      print "<dt>Rights</dt><dd>";
+		      foreach($solr_fields['mods_accessCondition_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	      if (isset($solr_fields['mods_location_physicalLocation_s']['value'])){
+		      print "<dt>Repository Information</dt><dd>";
+		      print "<p><strong>Repository: </strong>" . $solr_fields['mods_location_physicalLocation_s']['value'][0] . "</p>";
+		      print "<p><strong>Archival Collection: </strong>" . $solr_fields['mods_relatedItem_host_Collection_titleInfo_title_s']['value'][0] . "</p>";
+		      print "<p><strong>Collection Identifier: </strong>" . $solr_fields['mods_relatedItem_host_Collection_identifier_local_s']['value'][0] . "</p>";
+		      print "</dd>";
+	      }
+	      if (isset($solr_fields['mods_relatedItem_host_Project_titleInfo_title_s']['value'])){
+		      print "<dt>Digital Collection</dt><dd><p>" . $solr_fields['mods_relatedItem_host_Project_titleInfo_title_s']['value'][0] . "</p>";
+		      if (isset($solr_fields['mods_relatedItem_host_Project_titleInfo_title_s']['value'])){
+			      print $solr_fields['mods_relatedItem_host_Project_location_url_s']['value'][0];
+			  }
+			  print "</dd>";
+	      }
+	      if (isset($solr_fields['mods_physicalDescription_internetMediaType_s']['value'])){
+		      print "<dt>Internet Media Type</dt><dd><p>" . $solr_fields['mods_physicalDescription_internetMediaType_s']['value'][0] . "</p></dd>";
+	      }
+	      if (isset($solr_fields['mods_recordInfo_recordContentSource_ms']['value'])){
+		      print "<dt>Record Source</dt><dd>";
+		      foreach($solr_fields['mods_recordInfo_recordContentSource_ms']['value'] as $value){
+				  print "<p>" . $value . "</p>";
+		      }
+		      print "</dt>";
+	      }
+	  	?>
     </dl>
   </div>
 </fieldset>
